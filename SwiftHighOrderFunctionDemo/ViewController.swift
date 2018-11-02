b124caecb6fd4fb8eaa621d98aff42d00a80344e
Raghavendra Dattawad <raghavendra.d@wickedride.com>
2018-11-02 03:03:54 -0700

    Initial Commit

---
+++ SwiftHighOrderFunctionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftHighOrderFunctionDemo
+//
+//  Created by Raghavendra Dattawad on 11/2/18.
+//  Copyright © 2018 Raghavendra Dattawad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
